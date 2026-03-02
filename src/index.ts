91416779177d254d3c339d7fd21adb5d52ce1179
IdoKawaz <idohaker@gmail.com>
2026-03-02 17:36:30 +0200

    fix: correct multer export to use default export syntax

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Express as Application, Router, Request, Response, NextFunction } from "express";
-export { default as fileMiddleware } from "multer";
+export { default } from "multer";
 export { startServer } from "./server";
 export { createServerConfig, ServerConfig } from "./config";
 export { createRequestHandlerDecorator } from "./decorators";
